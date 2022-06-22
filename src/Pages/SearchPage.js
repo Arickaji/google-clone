cf5ba077eec9a73082ae12680a6ebaccab86fa4f
Aric kaji <arickaji007@gmail.com>
2022-06-22 22:05:34 +0530

    finalizing

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -15,9 +15,9 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 function SearchPage() {
 
     const [{term} , dispatch] = useStateValue();
-    // const { data } = useGoogleSearch(term); // LIVE API CALL
+    const { data } = useGoogleSearch(term); // LIVE API CALL
     
-    const data = response;
+    // const data = response; // MOCK API CALL
 
     return (
         <div className="searchPage">
@@ -66,11 +66,38 @@ function SearchPage() {
 
                     </div>
                 </div>
+            </div> 
 
-            </div> 
-            <div className="searchPage__results">
-                
-            </div> 
+            {term && (
+                <div className="searchPage__results">
+                    <p className="searchPage__resultCount">
+                        About {data?.searchInformation.formattedTotalResults} results ({data?.searchInformation.formattedSearchTime} seconds) for {term}
+                    </p>
+
+                    {data?.items.map(item => (
+                        <div className = "searchPage__result">
+                            <a href={item.link}>
+                                {item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src && (
+                                    <img className="searchPage__resultImage" src={ 
+                                            item.pagemap?.cse_image[0]?.src
+                                        }
+                                        alt=""
+                                    />
+                                )}
+                                {item.displayLink}
+                            </a>
+                            <a className="searchPage__resultTitle" href={item.link}>
+                                <h2>{item.title}</h2>
+                                <p className="searchPage__resultSnippet">
+                                    {item.snippet}
+                                </p>
+                            </a>
+                        </div>
+                    ))}
+
+                </div> 
+            )}
+
         </div>
     )
 }
